d51fb94785f1ce9e7605e47ad71781a676e47a4b
1565163445@qq.com <1565163445@qq.com>
2021-11-09 09:33:51 +0800

    update

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-05 15:06:14
- * @LastEditTime: 2021-11-09 09:32:07
+ * @LastEditTime: 2021-11-09 09:33:31
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\pagination\index.tsx
@@ -169,10 +169,7 @@ interface btnProps {
 
 function PaginationButton(Props: btnProps) {
   const { children, defaultCurrent, onClick: handleClick, lastPage } = Props;
-  // console.log(defaultCurrent === 1 && children === Left, "\r");
-  if (defaultCurrent === 1) {
-    console.log(children?.type, Left);
-  }
+
   return (
     <div
       className={[
