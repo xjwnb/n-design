f25ad173a515223cbbcc0c71f1294fe6cc59f698
1565163445@qq.com <1565163445@qq.com>
2021-11-09 11:24:08 +0800

    update

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-05 15:06:14
- * @LastEditTime: 2021-11-09 11:20:49
+ * @LastEditTime: 2021-11-09 11:23:30
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\pagination\index.tsx
@@ -107,7 +107,8 @@ function Pagination(Props: IProps) {
         };
       })
     );
-  }, [pageSizeOptions]);
+    // eslint-disable-next-line
+  }, []);
 
   /**
    * 点击页面
