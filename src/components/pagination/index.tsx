1c87e84bdca2852e5e74c5ac3e9b39e9e3be593c
1565163445@qq.com <1565163445@qq.com>
2021-11-17 10:49:24 +0800

    update

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-11-05 15:06:14
- * @LastEditTime: 2021-11-09 11:43:43
+ * @LastEditTime: 2021-11-16 09:48:24
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\pagination\index.tsx
  */
-import React, { useEffect, useState, MouseEventHandler } from "react";
+import React, { useEffect, useState, useCallback, MouseEventHandler } from "react";
 import Style from "./index.module.scss";
 import { Left, Right } from "../../Icons/icon/index";
 // components
@@ -141,13 +141,18 @@ function Pagination(Props: IProps) {
   /**
    * 选择器 change
    */
-  const handlePaginationSelectChange = function (value: string) {
-    let currentOption = selectOptions.find((item) => item.children === value);
-    setpageSize(Number(currentOption?.value));
-    setselectDefaultValue(Number(currentOption?.value));
-    onShowSizeChange &&
-      onShowSizeChange(Number(currentPage), Number(currentOption?.value));
-  };
+  const handlePaginationSelectChange = useCallback(
+    function (value: string) {
+      let currentOption = selectOptions.find((item) => item.children === value);
+      setpageSize(Number(currentOption?.value));
+      setselectDefaultValue(Number(currentOption?.value));
+      onShowSizeChange &&
+        onShowSizeChange(Number(currentPage), Number(currentOption?.value));
+      console.log("change")
+    },
+    // eslint-disable-next-line
+    [selectDefaultValue],
+  )
 
   /**
    * 输入框 blur
