61b6730b597279d9b729cfc94b053b12a0714813
1565163445@qq.com <1565163445@qq.com>
2021-11-22 08:46:28 +0800

    update

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-05 15:06:14
- * @LastEditTime: 2021-11-22 08:43:59
+ * @LastEditTime: 2021-11-22 08:46:10
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\pagination\index.tsx
@@ -153,7 +153,6 @@ function Pagination(Props: IProps) {
       setselectDefaultValue(Number(currentOption?.value));
       onShowSizeChange &&
         onShowSizeChange(Number(currentPage), Number(currentOption?.value));
-      console.log("change");
       handleClickPage(1);
     },
     // eslint-disable-next-line
