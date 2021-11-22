b66b1b159e3327fc322bcba57eb84d81f70df61b
1565163445@qq.com <1565163445@qq.com>
2021-11-22 16:28:11 +0800

    update

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-17 13:53:29
- * @LastEditTime: 2021-11-22 16:22:52
+ * @LastEditTime: 2021-11-22 16:24:34
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\form\index.tsx
@@ -105,20 +105,6 @@ const Form = function (Props: formProps) {
     });
   };
 
-  // console.log("reducter", state);
-
-  // useEffect(() => {
-  //   setformProvider({
-  //     labelCol,
-  //     wrapperCol,
-  //     // initValues: initVal,
-  //     initValues: state,
-  //     setFieldValue: setFieldValue,
-  //     handleFinish: handleFinish,
-  //   });
-  //   // eslint-disable-next-line
-  // }, []);
-
   return (
     <div className={[Style.n_form].join(" ")}>
       <FormContext.Provider
@@ -155,14 +141,6 @@ function Item(Props: itemProps) {
   // context
   const { labelCol, wrapperCol, initValues, setFieldValue, handleFinish } =
     useContext(FormContext);
-
-  // const [initState, setinitState] = useState(initValues);
-
-  // useEffect(() => {
-  //   setinitState(initValues);
-  // }, [initValues]);
-
-  // console.log(initValues);
 
   /**
    * 点击按钮
