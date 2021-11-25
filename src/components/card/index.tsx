c5f672a4104767fd85c2bdc3803c47209aa21447
1565163445@qq.com <1565163445@qq.com>
2021-11-25 11:15:55 +0800

    update

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-25 09:35:15
- * @LastEditTime: 2021-11-25 10:33:32
+ * @LastEditTime: 2021-11-25 11:15:42
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\card\index.tsx
@@ -56,7 +56,7 @@ function Card(Props: IProps) {
       )}
       {cover && <div className={Style.n_card_cover}>{cover}</div>}
 
-      {/* content */}
+      {/* body */}
       <div className={Style.n_card_body}>{children}</div>
     </div>
   );
