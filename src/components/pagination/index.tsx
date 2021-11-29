78ee6afd8f176810e04c8d9421fdcc058a86fef1
1565163445@qq.com <1565163445@qq.com>
2021-11-29 10:03:38 +0800

    update

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-05 15:06:14
- * @LastEditTime: 2021-11-25 15:23:56
+ * @LastEditTime: 2021-11-29 10:03:15
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\pagination\index.tsx
@@ -180,9 +180,11 @@ function Pagination(Props: IProps) {
 
   return (
     <div className={[Style.n_pagination].join(" ")}>
-      <div className={[Style.n_pagination_total].join(" ")}>
-        {showTotal && showTotal(total)}
-      </div>
+      {showTotal && (
+        <div className={[Style.n_pagination_total].join(" ")}>
+          {showTotal && showTotal(total)}
+        </div>
+      )}
       <div className={[Style.n_pagination_content].join(" ")}>
         <PaginationButton
           lastPage={lastPage}
