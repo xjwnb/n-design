5bd5c58ba619b5b7f64c26a1593b6f7a08703923
1565163445@qq.com <1565163445@qq.com>
2021-11-29 15:56:38 +0800

    READEME

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 08:53:10
- * @LastEditTime: 2021-11-29 15:14:45
+ * @LastEditTime: 2021-11-29 15:54:18
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\tooltip\index.tsx
