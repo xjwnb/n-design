a72d9245ce1c86164a2f046dfc32ee7a6a62d9a7
1565163445@qq.com <1565163445@qq.com>
2021-11-29 11:14:27 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-27 11:38:45
- * @LastEditTime: 2021-11-29 09:49:37
+ * @LastEditTime: 2021-11-29 11:13:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \n-design\src\App.tsx
@@ -1285,7 +1285,7 @@ function App() {
           }}
         >
           <Tooltip title="什么玩意">
-            <span>什么玩意儿</span>
+            <div>什么玩意儿</div>
           </Tooltip>
         </div>
         <div
@@ -1295,7 +1295,6 @@ function App() {
           }}
         >
           <Tooltip title="什么玩意">
-            {/* <span>什么玩意儿</span> */}
             <Button>什么玩意儿</Button>
           </Tooltip>
         </div>
--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 08:53:10
- * @LastEditTime: 2021-11-29 09:53:34
+ * @LastEditTime: 2021-11-29 11:00:16
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\tooltip\index.tsx
@@ -11,7 +11,7 @@
 import Style from "./index.module.scss";
 
 interface IProps {
-  children: React.ReactNode;
+  children: any;
   title: string;
 }
 
@@ -20,8 +20,8 @@ function Tooltip(Props: IProps) {
 
   return (
     <div className={Style.n_tooltip}>
-      <div className={Style.n_tooltip_children}>
-        {children}
+      <div className={Style.n_tooltip_content}>
+        <span className={Style.n_tooltip_children}>{children}</span>
         <span className={Style.n_tooltip_title}>{title}</span>
       </div>
     </div>
