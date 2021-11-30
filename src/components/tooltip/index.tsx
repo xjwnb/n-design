1607ea6d0e7e8c57d245c93183afa3dad5fc6943
1565163445@qq.com <1565163445@qq.com>
2021-11-30 09:10:51 +0800

    update 删除多余

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 08:53:10
- * @LastEditTime: 2021-11-30 09:04:26
+ * @LastEditTime: 2021-11-30 09:09:50
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\tooltip\index.tsx
@@ -31,9 +31,7 @@ interface IProps {
 function Tooltip(Props: IProps) {
   const { children, title, placement = "top" } = Props;
 
-  // const [offsetVal, setoffsetVal] = useState(0);
   const [top, settop] = useState<number | string>(0);
-  // const [height, setheight] = useState<number | string>(0);
   const [left, setleft] = useState<number | string>(0);
   const [showTip, setshowTip] = useState(false);
 
@@ -41,8 +39,6 @@ function Tooltip(Props: IProps) {
   const titleRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // console.log(childRef);
-    // let offsetWidth = childRef.current?.offsetWidth;
     childRef.current?.addEventListener("mousemove", () => {
       setshowTip(true);
     });
@@ -50,18 +46,11 @@ function Tooltip(Props: IProps) {
       setshowTip(false);
     });
     let titleOffsetWidth = titleRef.current?.offsetWidth;
-    // setoffsetVal(titleOffsetWidth || 0);
     if (["leftTop"].includes(placement)) {
       settop(0);
     } else if (["left", "right"].includes(placement)) {
       settop("-50%");
     } else if (["top"].includes(placement)) {
-      console.log(childRef.current?.offsetWidth, titleRef.current?.offsetWidth);
-      console.log(
-        ((childRef.current?.offsetWidth || 0) -
-          (titleRef.current?.offsetWidth || 0)) /
-          2
-      );
       setleft(
         ((childRef.current?.offsetWidth || 0) -
           (titleRef.current?.offsetWidth || 0)) /
@@ -77,7 +66,6 @@ function Tooltip(Props: IProps) {
     } else if (["topLeft", "topRight"].includes(placement)) {
       settop(-(childRef.current?.offsetHeight || 0) - 30);
     } else if (["rightBottom", "leftBottom"].includes(placement)) {
-      // settop()
       settop(
         (childRef.current?.offsetHeight || 0) -
           (titleRef.current?.offsetHeight || 0)
@@ -89,8 +77,6 @@ function Tooltip(Props: IProps) {
     if (["left", "leftBottom", "leftTop"].includes(placement)) {
       setleft(-(titleOffsetWidth || 0) - 20);
     } else if (["topRight", "bottomRight"].includes(placement)) {
-      // setleft("");
-
       setleft(
         (childRef.current?.offsetWidth || 0) -
           (titleRef.current?.offsetWidth || 0)
@@ -118,16 +104,9 @@ function Tooltip(Props: IProps) {
             placement !== "top" ? Style[`n_tooltip_title_${placement}`] : "",
           ].join(" ")}
           style={{
-            // opacity: showTip ? "1" : "0",
-            // display: showTip ? "block" : "none",
             visibility: showTip ? "visible" : "hidden",
             left: left,
-            // bottom: ["leftBottom"].includes(placement) ? 0 : "none",
-            top: top /* ["leftTop"].includes(placement)
-              ? 0
-              : ["left"].includes(placement)
-              ? "-50%"
-              : "", */,
+            top: top,
           }}
           ref={titleRef}
         >
