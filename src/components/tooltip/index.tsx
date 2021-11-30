5e67caa343f17fbc5046ee48f99c4b502cdf7d08
1565163445@qq.com <1565163445@qq.com>
2021-11-30 16:00:22 +0800

    update

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 08:53:10
- * @LastEditTime: 2021-11-30 15:41:45
+ * @LastEditTime: 2021-11-30 16:00:05
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\tooltip\index.tsx
@@ -38,7 +38,6 @@ function Tooltip(Props: IProps) {
 
   const childRef = useRef<HTMLElement>(null);
   const titleRef = useRef<HTMLElement>(null);
-
 
   useEffect(() => {
     childRef.current?.addEventListener("mousemove", () => {
