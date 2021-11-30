c287b69625c5fb8ff3dfcc034408b571015c42a6
1565163445@qq.com <1565163445@qq.com>
2021-11-30 10:09:41 +0800

    update

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-30 09:54:18
- * @LastEditTime: 2021-11-30 09:55:46
+ * @LastEditTime: 2021-11-30 09:59:14
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\carousel\index.tsx
@@ -17,8 +17,9 @@ function Carousel (Props: IProps) {
   console.log(children);
 
   return (
-    <>
-    </>
+    <div>
+      
+    </div>
   )
 }
 
