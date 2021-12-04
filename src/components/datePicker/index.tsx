c7153ec706f1c132f9f71e8e6a28efc1e10bd82e
1565163445@qq.com <1565163445@qq.com>
2021-12-04 13:32:09 +0800

    update

--- src/components/datePicker/index.tsx
+++ src/components/datePicker/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-03 15:13:35
- * @LastEditTime: 2021-12-04 11:55:03
+ * @LastEditTime: 2021-12-04 13:31:17
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\datePicker\index.tsx
@@ -205,7 +205,6 @@ function DatePicker(Props: IProps) {
           display: showPanel ? "block" : "none",
         }}
       >
-        {/* <PickerPanel day={dayArr} /> */}
         <Panel
           day={dayArr}
           firstIndex={firstIndex}
@@ -240,9 +239,6 @@ interface PanelProps {
 function PickerPanel(Props: PanelProps) {
   const { day, firstIndex, lastIndex, currentTime, nowTime } = Props;
 
-  // console.log(day, firstIndex, lastIndex);
-  console.log(currentTime, nowTime);
-
   const [preTime, setpreTime] = useState<{
     year: string | number;
     month: string | number;
@@ -267,10 +263,6 @@ function PickerPanel(Props: PanelProps) {
       year: preYear,
       month: preMonth,
     });
-    console.log({
-      year: preYear,
-      month: preMonth,
-    });
 
     let lastMonth = Number(currentTime.month) + 1;
     lastMonth = lastMonth > 12 ? 1 : lastMonth;
@@ -280,10 +272,6 @@ function PickerPanel(Props: PanelProps) {
         : currentTime.year;
 
     setlastTime({
-      year: lastYear,
-      month: lastMonth,
-    });
-    console.log({
       year: lastYear,
       month: lastMonth,
     });
