acf4a33989a2a87182e58f87a851600a679c6f55
1565163445@qq.com <1565163445@qq.com>
2021-12-09 16:05:56 +0800

    update

--- src/components/datePicker/index.tsx
+++ src/components/datePicker/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-03 15:13:35
- * @LastEditTime: 2021-12-09 16:02:59
+ * @LastEditTime: 2021-12-09 16:05:41
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\datePicker\index.tsx
