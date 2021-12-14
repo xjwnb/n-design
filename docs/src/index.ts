011ae625fe103b96b31045b5eb0a8056c88150f0
1565163445@qq.com <1565163445@qq.com>
2021-12-14 14:57:33 +0800

    update docs

--- docs/src/index.ts
+++ docs/src/index.ts
@@ -1,4 +1,3 @@
-export { default as Foo } from './Foo';
 export { default as Button } from "./button";
 // export { default as Icon } from "./Icons/icon/index.js";
 export { default as Divider } from "./divider";
