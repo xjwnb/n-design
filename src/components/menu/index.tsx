92ba0d801f93fdcd4da9f2c72991e64938fa55cb
1565163445@qq.com <1565163445@qq.com>
2021-12-23 17:12:28 +0800

    update

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-02 08:31:24
- * @LastEditTime: 2021-12-23 17:06:57
+ * @LastEditTime: 2021-12-23 17:08:45
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\menu\index.tsx
@@ -147,7 +147,7 @@ function SubMenu(Props: submenuProps) {
     } else if (theme === "dark") {
       seticonColor("#fff");
     }
-  }, [selectIdList, theme]);
+  }, [selectIdList, theme, id]);
 
   useEffect(() => {
     subMenuRef.current?.addEventListener("mouseover", () => {
