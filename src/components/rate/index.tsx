c21f9fcafe2430ca67dae3d9398266b499970de2
1565163445@qq.com <1565163445@qq.com>
2021-12-28 13:31:36 +0800

    update

--- src/components/rate/index.tsx
+++ src/components/rate/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-28 09:43:53
- * @LastEditTime: 2021-12-28 12:00:04
+ * @LastEditTime: 2021-12-28 13:29:16
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\rate\index.tsx
@@ -32,7 +32,8 @@ function Rate(Props: IProps) {
   }, [count]);
 
   useEffect(() => {
-    setval(value)
+    setval(value);
+    sethoverVal(value);
   }, [value]);
 
   useEffect(() => {
