1b1b0749646af8f3cb0e9503b1fb72980ef77d13
1565163445@qq.com <1565163445@qq.com>
2021-12-28 11:29:02 +0800

    update Rate

--- src/components/rate/index.tsx
+++ src/components/rate/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-28 09:43:53
- * @LastEditTime: 2021-12-28 11:03:53
+ * @LastEditTime: 2021-12-28 11:27:52
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\rate\index.tsx
@@ -10,13 +10,25 @@ import React, { useState, useEffect, useRef } from "react";
 import Style from "./index.module.scss";
 import { FivePointedStar } from "../../Icons/icon/index";
 
-interface IProps {}
+interface IProps {
+  allowHalf?: boolean;
+  count?: number;
+
+  onChange?(value: number): void;
+}
 
 function Rate(Props: IProps) {
+  const { allowHalf = false, count = 5, onChange } = Props;
+
   const [hoverVal, sethoverVal] = useState(0);
   const [val, setval] = useState(0);
+  const [countArr, setcountArr] = useState<number[]>([]);
 
   const ulRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    setcountArr(Array.from(new Array(count).keys()));
+  }, [count]);
 
   useEffect(() => {
     ulRef.current?.addEventListener("mousemove", (e: any) => {
@@ -37,29 +49,34 @@ function Rate(Props: IProps) {
    */
   const handleClickLi = function (value: number) {
     setval(value);
+    onChange?.(value);
   };
 
   return (
     <div className={[Style.n_rate].join(" ")}>
       <ul ref={ulRef}>
-        {[0, 1, 2, 3, 4].map((item) => {
-          return (
-            <li key={item} onClick={() => handleClickLi(item + 1)}>
-              <span
-                data-index={item + 1}
-                className={[
-                  hoverVal === item + 1 ? Style.n_rate_span_active : "",
-                ].join(" ")}
-              >
-                <FivePointedStar
+        {countArr.map((item) => {
+          if (!allowHalf) {
+            return (
+              <li key={item} onClick={() => handleClickLi(item + 1)}>
+                <span
                   data-index={item + 1}
-                  width={20}
-                  height={20}
-                  color={hoverVal >= item + 1 ? "#FADB14" : "#F0F0F0"}
-                />
-              </span>
-            </li>
-          );
+                  className={[
+                    hoverVal === item + 1 ? Style.n_rate_span_active : "",
+                  ].join(" ")}
+                >
+                  <FivePointedStar
+                    data-index={item + 1}
+                    width={20}
+                    height={20}
+                    color={hoverVal >= item + 1 ? "#FADB14" : "#F0F0F0"}
+                  />
+                </span>
+              </li>
+            );
+          } else {
+            return <></>;
+          }
         })}
       </ul>
     </div>
