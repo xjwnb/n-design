9c7e6aeb681e9f0d4ec4e85b03fa43bff1108a52
1565163445@qq.com <1565163445@qq.com>
2021-12-30 14:04:56 +0800

    update

--- src/components/rate/index.tsx
+++ src/components/rate/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-28 09:43:53
- * @LastEditTime: 2021-12-28 14:13:30
+ * @LastEditTime: 2021-12-29 13:36:17
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \n-design\src\components\rate\index.tsx
